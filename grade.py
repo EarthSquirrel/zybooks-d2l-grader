2508ec3f2233df570072df1e97cbf23cccf97ecd
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-09-08 11:54:31 -0600

    Get zybook grade column with KeyError

--- grade.py
+++ grade.py
@@ -2,8 +2,8 @@ import pandas as pd
 import sys
 
 zybook_csv_path = sys.argv[1]
-zybook_column = sys.argv[2]
-d2l_column = sys.argv[3]
+zybook_col = sys.argv[2]
+d2l_col = sys.argv[3]
 d2l_saved_name = sys.argv[4]
 
 # get usernames for class
@@ -15,7 +15,13 @@ with open(class_list_path, 'r') as f:
         class_list.append(line.strip())
         line = f.readline()
 
-print(class_list)
 
 # read in zybooks
 zybook_df = pd.read_csv(zybook_csv_path)
+try:
+    zybook_grade_col = zybook_df[zybook_col]
+except KeyError:
+    print("****** KeyError ******")
+    print('"{}" is not a column in the zybooks csv'.format(zybook_col))
+    print('**********************')
+    print('Column options are: {}'.format(', '.join(zybook_df.columns.values)))
