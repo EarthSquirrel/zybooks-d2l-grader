6bc367083cd6b41493e9c19c4b48e35025a0aa5b
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-09-08 12:43:43 -0600

    Get list of missing from d2l and zybooks

--- build_mapping.py
+++ build_mapping.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sys
+import re
 
 # paths to files from command line input
 zybook_csv_path = sys.argv[1]
@@ -12,3 +13,37 @@ with open(d2l_class_list_path, 'r') as f:
     while line:
         d2l_class_list.append(line.strip())
         line = f.readline()
+
+
+# read in zybooks
+zybook_df = pd.read_csv(zybook_csv_path)
+
+# pair zybook name with d2l usernames
+matched = []   # d2l usernames with matched zybook name
+missed_zybooks_users = []
+# go through each row in zybooks and find corresponding d2l username
+for index, row in zybook_df.iterrows():
+    added = False  # bool to tell if zybooks name matched d2l username
+    # compare last name to d2l usernames
+    for student in d2l_class_list:
+        student = student.lower()
+        last_name = row['Last name'].lower()
+        last_name = re.sub(r'\W+', '', last_name)
+        if last_name in student:
+            first_name = row['First name'].lower()
+            first_name = re.sub(r'\W+', '', first_name)
+            if first_name in student:
+                matched.append([student, row['First name'], row['Last name']])
+                added = True
+    if not added:
+        missed_zybooks_users.append('{} {}'.format(row['First name'],
+                                                   row['Last name']))
+
+print('Zybook users not found:')
+print(missed_zybooks_users)
+
+print('D2L users not found:')
+success_d2l = [match[0] for match in matched]
+# s.difference(t)   s - t   new set with elements in s but not in t
+missing_d2l = list(set(d2l_class_list).difference(set(success_d2l)))
+print(missing_d2l)
