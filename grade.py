4f3b5cd6f230da651e5ed0a7de86ae33ebac60a6
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-09-08 11:45:20 -0600

    Get class list from .txt file and read in

--- grade.py
+++ grade.py
@@ -6,7 +6,16 @@ zybook_column = sys.argv[2]
 d2l_column = sys.argv[3]
 d2l_saved_name = sys.argv[4]
 
+# get usernames for class
+class_list_path = "classlist.txt"
+class_list = []
+with open(class_list_path, 'r') as f:
+    line = f.readline()
+    while line:
+        class_list.append(line.strip())
+        line = f.readline()
+
+print(class_list)
 
 # read in zybooks
 zybook_df = pd.read_csv(zybook_csv_path)
-print(zybook_df[zybook_column])
