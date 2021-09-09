fed546d1b58fa295d49231836d381e50d3ee3bf4
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-09-08 23:02:29 -0600

    CHanged order to make more sense

--- gui_grade.py
+++ gui_grade.py
@@ -3,22 +3,26 @@ from tkinter.filedialog import askopenfilename
 import os
 #from gui_functions import *
 
+# Variables that do things
+zybook_csv_path = ''
+
+
+
 root = tk.Tk()
 
 # label to display file name
-zybook_csv_path = ''
 uploaded_file_str = tk.StringVar()
-def upload_file():
-    # show an "Open" dialog box and return the path to the selected file
-    zybook_csv_path = askopenfilename() 
-    uploaded_file_str.set(os.path.split(zybook_csv_path)[1])
-
-
 label_uploaded_file = tk.Label(root, textvariable=uploaded_file_str)
 uploaded_file_str.set('select a file to upload')
 label_uploaded_file.grid(row=1, columnspan=2, sticky='')
 
+
 # controls to upload a file
+def upload_file():
+    print('Calling: upload_file')
+    # show an "Open" dialog box and return the path to the selected file
+    zybook_csv_path = askopenfilename() 
+    uploaded_file_str.set(os.path.split(zybook_csv_path)[1])
 label_upload_file = tk.Label(root, text="zyBook csv file: ")
 label_upload_file.grid(row=0, column=0)
 btn_upload_file = tk.Button(root, text="Upload File", command=upload_file)
