5cc4973918698a8bf0cefa1f875ee0234787a6c0
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-09-08 23:48:09 -0600

    Removed bad planning file

--- gui_functions.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from tkinter.filedialog import askopenfilename
-
-def upload_file():
-    # show an "Open" dialog box and return the path to the selected file
-    filename = askopenfilename() 
-    print(filename)
-
